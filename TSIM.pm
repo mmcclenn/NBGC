bd80e76fa65bba49c0284284137aba5af4373712
Michael McClennen <michaelm@umich.edu>
2010-06-18 23:22:50 -0500

    end of day 6/18

--- TSIM.pm
+++ TSIM.pm
@@ -8,11 +8,37 @@
 package TSIM;
 use strict;
 
+# How to use this package:
+# 
+# Running a model consists of 5 phases, all of which are optional except the first.
+# 
+# 1. load	read in the model and set up the framework necessary to run it
+# 2. init	initialize the model's variables
+# 3. spinup	run the model long enough to establish a steady state
+# 4. run	run the model for a specified number of steps, or until a specified
+#		condition is reached
+# 5. output	write the data to disk
+# 
+# Here is an example:
+# 
+# $sim = TSIM->new('simvar');
+# $simvar::some_variable = 3;
+# $simvar::some_flag = 1;
+# $sim->load_from("model.txt");
+# $sim->init();
+# $simvar::var2 = 5;
+# $sim->spinup();
+# $simvar::some_flag = 0;
+# $sim->sample(1, qw(var1 var2 var3));
+# $sim->run(300);
+# $sim->write_data(OUTFILE);
+# $sim->plot_var('var1_plot.png', 'var1');
+
 
 # Constructor, destructor and related methods
 # ===========================================
 
-# new : Create a new instance.
+# new : Create a new simulation instance, with a given name space.
 
 sub new {
 
@@ -20,24 +46,18 @@ sub new {
     my $self = {};
     bless $self, $class;
     
-    $self->initialize();
+    my $namespace = shift || '__SIM';
     
-    return $self;
-}
-
-
-# initialize : Initialize a new instance
-
-sub initialize {
-
-    my $self = shift;
+    $self->{namespace} = $namespace;
     
     $self->{VTABLE} = {};
     $self->{ITABLE} = [];	# list of initialization statements in Perl
     $self->{STABLE} = [];
     
     $self->{VINDEX} = 0;
-}
+    
+    return $self;
+}    
 
 
 # empty destructor
@@ -46,12 +66,77 @@ sub DESTROY {
 }
 
 
-# Now, routines for parsing statements that define the model.
+# Routines for phase 1: load
 # ===========================================================
 
-# add_init_stmt : Parse an initialization statement, and add it to the
+# load : Load a list of statements that define a model.
+
+sub load {
+
+    foreach my $line (@_)
+    {
+	next unless $line =~ /\S/;     	# ignore blank lines
+	s/^\s*//;			# take out initial whitespace
+	s/\s*$//;			# take out final whitespace
+	
+	if ( $line =~ / ^ init \s+ (.*) /xoi )
+	{
+	    &parse_init_stmt($1);
+	}
+	
+	else
+	{
+	    &parse_step_stmt($line);
+	}
+    }
+}
+
+
+# load_from : Load statements from a file or file handle.
+
+sub load_from {
+
+    my $arg = shift;
+    
+    # if $arg is a file handle, read from it.
+    
+    # otherwise, open the named file and read from that.
+}
+
+
+
+# parse_init_stmt : Parse an initialization statement, and add it to the
 # model.  These are run (as Perl code) before the model is started up,
 # to initialize variables.
+
+sub parse_init_stmt {
+
+    my ($self, $what) = @_;
+    my $const;
+
+    if ( $what =~ / ^ const \s+ (.*) /xoi )
+    {
+	$what = $1;
+	$const = 1;
+    }	
+
+    if ( $what =~ / ^ \$(\w+) \s* = \s* (.*) /xoi )
+    {
+	$self->add_init_stmt(name => $1, expr => $2, const => $const);
+    }
+    
+    elsif ( $what =~ / ^ \$(\w+) $ /xo )
+    {
+	$self->add_init_stmt(name => $1, const => $const);
+    }
+    
+    else
+    {
+	die "Could not parse statement: $what\n";
+    }
+}
+
+# add_init_stmt : Actually add the init code, and mark certain variables as constant.
 
 sub add_init_stmt {
 
@@ -65,8 +150,47 @@ sub add_init_stmt {
     if (defined $args{expr})
     {
 	push @ITABLE, "$name = $expr;\n";
-    }
+    }    
+}
+
+
+sub parse_step_stmt {
     
+    my ($self, $what) = @_;
+    
+    given ($what)
+    {
+	when ( / ^ \$ (\w+) \s* -> \s* \$ (\w+) \s* : 
+		 \s* \$ (\w+) \s* \* \s* \$ (\w+) $ /xo )
+	{
+	    $self->add_step_stmt(source => $1, sink => $2, rate1 => $3, rate2 => $4);
+	}
+	
+	when ( / ^ \$ (\w+) \s* -> \s* \$ (\w+) \s* : \s* \$ (\w+) $ /xo )
+	{
+	    $self->add_step_stmt(source => $1, sink => $2, rate1 => $3, rate2 => 1);
+	}
+	
+	when ( / ^ \$ (\w+) \s* -> \s* \$ (\w+) \s* : \s* (.*) /xo )
+	{
+	    $self->add_step_stmt(source => $1, sink => $2, rate_expr => $3);
+	}
+	
+	when ( / ^ \$ (\w+) \s* \* \s* \$ (\w+) \s* -> \s* \$ (\w+) $ /xo )
+	{
+	    $self->add_step_stmt(source => $1, sink => $3, rate1 => $1, rate2 => $2);
+	}
+	
+	when ( / ^ \$ (\w+) \s* -> \s* \$ (\w+) \s* \* \s* \$ (\w+) $ /xo )
+	{
+	    $self->add_step_stmt(source => $1, sink => $3, rate1 => $3, rate2 => $2);
+	}	
+	
+	default
+	{
+	    die "Could not parse statement: $what\n";
+	}
+    }
 }
 
 
@@ -83,6 +207,11 @@ sub add_step_stmt {
     
     given ($source)
     {
-	where (/^\w+$/) {
+	when ('ENDLESS') {
+	    die "You cannot use a proportional flow from an endless source\n"
+		if $args{type} eq 'PROP';
+	    
+	
+	when (/^\w+$/) {
 	    $source_id = $self->lookup_var($_);
 	    
--- tsim.pl
+++ tsim.pl
@@ -13,119 +13,15 @@ use TSIM.pm;
 use TVAR.pm;
 
 
-my $system = new TSIM;
+my $sim = TSIM->new('simvar');
 
 while (<>)
 {
-    s/#.*//;			# take out comments
-    next unless /\w/;		# ignore blank lines
-    s/^\s*//;			# take out initial whitespace
-    s/\s*$//;			# take out final whitespace
-    
-    my ($when, $what) = split /\s+/, $_, 2;
-    
-    given (uc $when)
-    {
-	when ('INIT') {
-	    &parse_init_stmt($what);
-	}
-
-	when ('STEP') {
-	    &parse_step_stmt($what);
-	}
-    }
+    $sim->load($_);
 }
+
 
 my $a = 0;
 
 
-    
-sub parse_init_stmt {
 
-    my ($what) = @_;
-    
-    given ($what) 
-    {
-	when ( /^ const (\w+) \s* = \s* (.*) $/xoi )
-	{
-	    $system->add_init_stmt(name => $1, expr => $2, const => 1);
-	}
-	
-	when ( /^ (\w+) \s* = \s* (.*) $/xo )
-	{
-	    $system->add_init_stmt(name => $1, expr => $2);
-	}
-	
-	when ( /^ const (\w+) $/xoi )
-	{
-	    $system->add_init_stmt(name => $1, const => 1);
-	}
-	
-	when (/(\w+)\s*$/)
-	{
-	    $system->add_init_stmt(name => $1);
-	}
-    }
-}
-
-
-sub parse_step_stmt {
-    
-    my ($what) = @_;
-    
-    given ($what)
-    {
-	when ( /^ (\w+) \s* -> \s* \[(\w+)\] \s* (\w+) $/xo )
-	{
-	    $system->add_step_stmt(source => $1, rate => $2, sink => $3,
-				   type => 'STRAIGHT');
-	}
-	
-	when ( /^ (\w+) \s* \[(\w+)\] \s* -> \s* (\w+) $/xo )
-	{
-	    $system->add_step_stmt(source => $1, rate => $2, sink => $3,
-				   type => 'STRAIGHT');	
-	}
-	
-	when ( /^ (\w+) \s* -> \s* \((\w+)\) \s* (\w+) $/xo )
-	{
-	    $system->add_step_stmt(source => $1, rate => $2, sink => $3,
-				   type => 'PROP');
-	}
-	
-	when ( /^ (\w+) \s* \((\w+)\) \s* -> \s* (\w+) $/xo )
-	{
-	    $system->add_step_stmt(source => $1, rate => $2, sink => $3,
-				   type => 'PROP');	
-	}
-	
-	when ( /^ (\w+) \s* <- \s* \[(\w+)\] \s* (\w+) $/xo )
-	{
-	    $system->add_step_stmt(source => $3, flow => $2, sink => $1,
-				   type => 'STRAIGHT');
-	}
-	
-	when ( /^ (\w+) \s* \[(\w+)\] \s* <- \s* (\w+) $/xo )
-	{
-	    $system->add_step_stmt(source => $3, flow => $2, sink => $1, 
-				   type => 'STRAIGHT');
-	}
-	
-	when ( /^ (\w+) \s* <- \s* \((\w+)\) \s* (\w+) $/xo )
-	{
-	    $system->add_step_stmt(source => $3, flow => $2, sink => $1,
-				   type => 'PROP');
-	}
-	
-	when ( /^ (\w+) \s* \((\w+)\) \s* <- \s* (\w+) $/xo )
-	{
-	    $system->add_step_stmt(source => $3, flow => $2, sink => $1, 
-				   type => 'PROP');
-	}
-	
-	default
-	{
-	    die "Could not parse statement: $what\n";
-	}
-    }
-}
